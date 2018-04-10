0cba8b5534542f9284b6e3618fde1a31a35f9552
juaquinsan <juakisanchez@gmail.com>
2018-04-10 14:03:59 +0200

    CRUD Supplier

--- app/controllers/admin/supplier_controller.rb
+++ app/controllers/admin/supplier_controller.rb
@@ -1,22 +1,55 @@
 class Admin::SupplierController < ApplicationController
   def new
+    @supplier = Supplier.new
+    @page_title = 'Create new supplier'
   end
 
   def create
+    @supplier = Supplier.new(supplier_params)
+    if @supplier.save
+      flash[:notice] = "Supplier #{@supplier.name} was succesfully created."
+      redirect_to :action => 'index'
+    else
+      @page_title = 'Create new supplier'
+      render :action => 'new'
+    end
   end
 
   def edit
+    @supplier = Supplier.find(params[:id])
+    @page_title = 'Edit supplier'
   end
 
   def update
+    @supplier = Supplier.find(params[:id])
+    if @supplier.update_attributes(supplier_params)
+      flash[:notice] = "Supplier #{@supplier.name} was succesfully updated."
+      redirect_to :action => 'show', :id => @supplier
+    else
+      @page_title = 'Edit supplier'
+      render :action => 'edit'
+    end
   end
 
   def destroy
+    @supplier = Supplier.find(params[:id])
+    @supplier.destroy
+    flash[:notice] = "Succesfully deleted supplier #{@supplier.name}."
+    redirect_to :action => 'index'
   end
 
   def show
+    @supplier = Supplier.find(params[:id])
+    @page_title = @supplier.name
   end
 
   def index
+    @suppliers = Supplier.all
+    @page_title = 'Listing suppliers'
   end
+
+  private
+    def supplier_params
+      params.require(:supplier).permit(:first_name, :last_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'admin/supplier/show/:id' => 'admin/supplier#show'
   get 'admin/supplier/index'
   get 'admin/about/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
